83eca3b2bd64499deddd6fb2a99fe35829fa157d
reya <reya@lume.nu>
2024-11-08 13:20:54 +0700

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,13 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				serif: ["Alice", ...defaultTheme.fontFamily.serif],
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hr: "-webkit-linear-gradient(0deg, transparent, #eab308, #ef4444, #ec4899, #a855f7, #3b82f6, transparent)",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
